990b124b19d4f387239051c5f4a6f305e9dad01a
defauth98 <neto.daniribeiro@gmail.com>
2021-07-10 10:16:05 -0300

    feat: tela de login

---
+++ src/pages/admin.tsx
@@ -0,0 +1,9 @@
+const Admin: React.FC = () => {
+  return (
+    <main>
+      <h1>Admin PAge</h1>
+    </main>
+  );
+}
+
+export default Admin;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,37 @@
+import { useRouter } from 'next/router'
+import { useForm } from "react-hook-form";
+
 export default function Home() {
+  const router = useRouter();
+  const { register, handleSubmit, formState: { errors } } = useForm();
+ 
+  function onSubmit(data) {  
+    const {email, password} = data;
+
+    if(email && password) {
+      router.push('/admin')
+    }
+  }
+
   return (
-   <h1>Main page</h1>
+   <main>
+     <form onSubmit={handleSubmit(onSubmit)}>
+      <input 
+        type="email" 
+        name="email" 
+        id="email"
+        {...register("email", { required: true })} />
+        {errors.email && <span>This field is required</span>}
+
+      <input 
+        type="password"
+        name="password" 
+        id="password"
+        {...register("password", { required: true })} />
+        {errors.password && <span>This field is required</span>}
+
+      <button type="submit">Entrar</button>
+     </form>
+   </main>
   )
 }
